983ef3213a26277d8a90842c6d84f3163f117cfe
mini-militant <shailesh.coolcalm007@gmail.com>
2019-09-17 13:36:56 +0530

    1 file modified

--- components/ReactTutorial/Environment.js
+++ components/ReactTutorial/Environment.js
@@ -49,17 +49,28 @@ const ReactEnvironment = (props) => {
   return (
 
     <div>
-      <br/>
+      <br />
       <Paper className={classes.paper}>
-        <br/>
+        <br />
         <h2 className={classes.root}>React - Environment Setup</h2>
         <Divider />
-        <br/>
+        <br />
         <div className={classes.content}>
           <section>
             In this section we will see the setup for successful React development. There are a few prerequisites for setting up the react pojects. We will need NodeJS, so if you have not installed click on the below link.
       </section>
           <br />
+          <section>
+            <h4>Requirements</h4>
+            <section>
+              <p>The Create React App is maintained by <b>Facebook</b> and can works on any <b>platform</b>, for example, macOS, Windows, Linux, etc. To create a React Project using create-react-app, you need to have installed the following things in your system.</p>
+              <ol>
+               <li>Node version >= 8.10</li>
+               <li>NPM version >= 5.6</li>
+              </ol>
+            </section>
+          </section>
+
           <section>
             <h4><a href="https://nodejs.org/en/">NodeJS Environment Setup</a></h4>
           </section>
@@ -79,7 +90,9 @@ const ReactEnvironment = (props) => {
             <div className={classes.box}>
               npm start
           </div>
-
+          <section>
+          <p>Now, open the <b>src >> App.js</b> file and make changes which you want to display on the screen. After making desired changes, save the file. As soon as we save the file, Webpack recompiles the code, and the page will refresh automatically, and changes are reflected on the browser screen. Now, we can create as many components as we want, import the newly created component inside the App.js file and that file will be included in our main index.html file after compiling by Webpack.</p>
+          </section>
           </section>
           <br />
         </div>
